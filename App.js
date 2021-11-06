4375a2cd7373dc6e73ce5d20c8ab6e0b2fb1e44c
Maneeka Mishra <maneekamishr@umass.edu>
2021-11-05 22:50:41 -0400

    added buttons

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, Button, View, DatePickerAndroid } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View>
+        <Button title="Login"></Button>
+      </View>
+
+      <View>
+        <Button title="Register"></Button>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: 'Pink',
+
+  }
 });
